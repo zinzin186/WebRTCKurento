b27f56fc36146142734fc3a17c57324b117a0f72
Din Vu Dinh <>
2020-07-02 23:42:40 +0700

    edit

---
+++ WebRTCKurento/WebRTCKurento/WebRTCKurento.h
@@ -0,0 +1,19 @@
+//
+//  WebRTCKurento.h
+//  WebRTCKurento
+//
+//  Created by Admin on 7/2/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebRTCKurento.
+FOUNDATION_EXPORT double WebRTCKurentoVersionNumber;
+
+//! Project version string for WebRTCKurento.
+FOUNDATION_EXPORT const unsigned char WebRTCKurentoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebRTCKurento/PublicHeader.h>
+
+
